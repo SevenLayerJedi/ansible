54890b2f16a72a0c71ca5fe4e8d7520bf72febe2
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-03-24 10:26:02 -0600

    Commit to main

--- registry/bbt-website/init.sh
+++ registry/bbt-website/init.sh
@@ -1,33 +1,12 @@
 #!/bin/sh
 
 # define path to custom docker environment
-DOCKER_ENVVARS=/etc/apache2/docker_envvars
+#DOCKER_ENVVARS=/etc/apache2/docker_envvars
 
 # write variables to DOCKER_ENVVARS
-cat << EOF > "$DOCKER_ENVVARS"
 export APACHE_RUN_USER=www-data
 export APACHE_RUN_GROUP=www-data
 export APACHE_LOG_DIR=/var/log/apache2
 export APACHE_LOCK_DIR=/var/lock/apache2
 export APACHE_PID_FILE=/var/run/apache2.pid
 export APACHE_RUN_DIR=/var/run/apache2
-EOF
-
-# source environment variables to get APACHE_PID_FILE
-. "$DOCKER_ENVVARS"
-
-# only delete pidfile if APACHE_PID_FILE is defined
-if [ -n "$APACHE_PID_FILE" ]; then
-   rm -f "$APACHE_PID_FILE"
-fi
-
-# start other services
-service exim4 start
-service dovecot start
-service fetchmail start
-
-# line copied from /etc/init.d/apache2
-ENV="env -i LANG=C PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-# use apache2ctl instead of /usr/sbin/apache2
-$ENV APACHE_ENVVARS="$DOCKER_ENVVARS" apache2ctl -DFOREGROUND
