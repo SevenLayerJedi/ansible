7476896cffd2c573751b6b12a1f0c4a4e6d61777
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-03-31 12:32:59 -0600

    Commit to main

--- registry/bbt-website/www/index.php
+++ registry/bbt-website/www/index.php
@@ -1,29 +1,51 @@
-<?php 
-    session_start();
-    if (isset($_SESSION['id']) && isset($_SESSION['user_name'])){
-    header("Location: home.php");
-    exit();
-    }
-?>
-
-
+<?php include "db_conn.php"; ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>LOGIN</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <title>SB Admin - Start Bootstrap Template</title>
+
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+ <link href="css/sb-admin.css" rel="stylesheet">
 </head>
-<body>
-     <form action="login2.php" method="post">
-        <h2>LOGIN</h2>
-        <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?></p>
-        <?php } ?>
-        <label>User Name</label>
-        <input type="text" name="uname" placeholder="User Name"><br>
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password"><br> 
-        <button type="submit">Login</button>
-     </form>
+<body class="bg-dark">
+ <div class="container">
+   <div class="card card-login mx-auto mt-5">
+     <div class="card-header">Login</div>
+     <div class="card-body">
+       <form method="post" action="login2.php">
+          <?php include "errors.php"; ?>
+         <div class="form-group">
+           <label for="exampleInputEmail1">Username</label>
+           <input class="form-control"  type="text" name="uname">
+         </div>
+         <div class="form-group">
+           <label for="exampleInputPassword1">Password</label>
+           <input class="form-control"  type="password" name="password">
+         </div>
+         <div class="form-group">
+           <div class="form-check">
+             <label class="form-check-label">
+               <input class="form-check-input" type="checkbox"> Remember Password</label>
+           </div>
+         </div>
+         <button type="submit" class="btn btn-primary btn-block" name="login_user">Login</button>
+       </form>
+       <div class="text-center">
+         <a class="d-block small mt-3" href="register.php">Register an Account</a>
+      <!-- <a class="d-block small" href="forgot-password.php">Forgot Password?</a>-->
+       </div>
+     </div>
+   </div>
+ </div>
+
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.bundle.min.js"></script>
+ <script src="js/jquery.easing.min.js"></script>
 </body>
 </html>
