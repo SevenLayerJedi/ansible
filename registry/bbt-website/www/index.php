6a6e7bfe9203b8c07eb96f4a671648f498f0e2c6
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-03-31 12:34:53 -0600

    Commit to main

--- registry/bbt-website/www/index.php
+++ registry/bbt-website/www/index.php
@@ -19,7 +19,10 @@
      <div class="card-header">Login</div>
      <div class="card-body">
        <form method="post" action="login2.php">
-          <?php include "errors.php"; ?>
+          <!-- <?php include "errors.php"; ?> -->
+          <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
          <div class="form-group">
            <label for="exampleInputEmail1">Username</label>
            <input class="form-control"  type="text" name="uname">
