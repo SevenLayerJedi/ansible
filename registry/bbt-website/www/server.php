30f29f2c5462d6a932ddf387fa498a45d5d341b6
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-03-31 10:31:15 -0600

    Commit to main

--- registry/bbt-website/www/server.php
+++ registry/bbt-website/www/server.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 // initializing variables
-$username = "admin";
-$email    = "admin";
-$db_server = "10.200.1.91";
-$dbname = "bbt";
+$username = "";
+$email    = "";
 $errors = array();
 // connect to the database
-$db = mysqli_connect($servername, $username, $password, $dbname);
+$db = mysqli_connect('10.200.1.91', 'admin', 'admin', 'bbt');
