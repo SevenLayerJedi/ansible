926168ad64a7dd53f7ca268334eab1a09b9d55c3
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-04-06 09:56:37 -0600

    Commit to main

--- registry/bbt-website/www/home.php
+++ registry/bbt-website/www/home.php
@@ -292,7 +292,7 @@
         </script>
 
 
-    <!-- Example Job Info Card-->
+    <!-- Job Activity Card-->
     <div class="card mb-3">
         <div class="card-header">
         <i class="fa fa-table"></i> Job Activity</div>
@@ -361,6 +361,77 @@
         </div>
         </div>
 
+
+
+
+    <!-- Job Activity Card-->
+    <div class="card mb-3">
+        <div class="card-header">
+        <i class="fa fa-table"></i> Job Activity</div>
+        <div class="card-body">
+        <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name of Product</th>
+            <th>Price of Product</th>
+            <th>Product Catrogy</th>
+            <th>Product Details</th>
+        </tr>
+        </thead>
+        <?php
+
+        $servername = "10.200.1.91";
+        $username = "admin";
+        $password = "admin";
+        $dbname = "bbt";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $sql = "SELECT * from products";
+        if (mysqli_query($conn, $sql)) {
+            echo "";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        $count = 1;
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tbody>
+            <tr>
+                <th>
+                    <?php echo $row["product_id"]; ?>
+                </th>
+                <td>
+                    <?php echo $row["product_name"]; ?>
+                </td>
+                <td>
+                    <?php echo $row["product_price"]; ?>
+                </td>
+                <td>
+                    <?php echo $row["product_cat"]; ?>
+                </td>
+                <td>
+                    <?php echo $row["product_details"]; ?>
+                </td>
+            </tr>
+        </tbody>
+
+        <?php $count++;}
+        } else {
+            echo "0 results";
+        }
+        ?>
+
+        </table>
+        </div>
+        </div>
+        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
+        </div>
 
 
 
