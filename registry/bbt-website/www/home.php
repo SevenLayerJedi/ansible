ee3199d83e38d3a9a2a48e24b992d9d710fe7637
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-04-06 11:06:33 -0600

    Commit to main

--- registry/bbt-website/www/home.php
+++ registry/bbt-website/www/home.php
@@ -262,7 +262,7 @@
         </div>
         </div>
 
-        <!-- New Code -->
+        <!-- Update Table Code -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
         $(document).ready(function(){
