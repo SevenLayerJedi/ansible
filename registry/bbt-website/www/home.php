1c8e0fa01899c6e16549fbe91a9dc901028daa91
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-04-06 08:14:33 -0600

    Commit to main

--- registry/bbt-website/www/home.php
+++ registry/bbt-website/www/home.php
@@ -203,8 +203,8 @@
         <i class="fa fa-fw fa-comments"></i>
         </div>
 
-
-        <div class="mr-5"><?php echo $row["Vistors"]; ?> Vistors</div>
+        <!--  Total Jobs Completed -->
+        <div class="mr-5"><?php echo $row["Vistors"]; ?> Total Jobs Completed</div>
         </div>
         <a class="card-footer text-white clearfix small z-1" href="#">
         <span class="float-left">View Details</span>
@@ -221,8 +221,8 @@
         <i class="fa fa-fw fa-list"></i>
         </div>
 
-
-        <div class="mr-5"><?php echo $row["revenue"]; ?>  Revenue</div>
+        <!-- Jobs in Queue -->
+        <div class="mr-5"><?php echo $row["revenue"]; ?>  Jobs in Queue</div>
         </div>
         <?php }
         } else {
@@ -237,13 +237,15 @@
         </a>
         </div>
         </div>
-        <div class="col-xl-3 col-sm-6 mb-3">
+        <div class="col-xl-4 col-sm-6 mb-4">
         <div class="card text-white bg-success o-hidden h-100">
         <div class="card-body">
         <div class="card-body-icon">
         <i class="fa fa-fw fa-shopping-cart"></i>
         </div>
-        <div class="mr-5">123 New Orders!</div>
+
+        <!-- Active Jobs -->
+        <div class="mr-5">4 Active Jobs</div>
         </div>
         <a class="card-footer text-white clearfix small z-1" href="#">
         <span class="float-left">View Details</span>
@@ -253,30 +255,16 @@
         </a>
         </div>
         </div>
-        <div class="col-xl-3 col-sm-6 mb-3">
+        <div class="col-xl-4 col-sm-6 mb-4">
         <div class="card text-white bg-danger o-hidden h-100">
         <div class="card-body">
         <div class="card-body-icon">
         <i class="fa fa-fw fa-support"></i>
         </div>
-        <div class="mr-5">13 New Tickets!</div>
-        </div>
-        <a class="card-footer text-white clearfix small z-1" href="#">
-        <span class="float-left">View Details</span>
-        <span class="float-right">
-        <i class="fa fa-angle-right"></i>
-        </span>
-        </a>
-        </div>
-        </div>
-        </div>
-        <?php
-
-
-
-        // End of FIrst Section Cards
         
-
+        <!-- End of First Section Cards -->
+        
+        <?php
         $servername = "10.200.1.91";
         $username = "admin";
         $password = "admin";
