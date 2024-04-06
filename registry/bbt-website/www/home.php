7a6660f88d5de0af1e329a5075f8b5ad525fc65a
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-04-06 08:15:58 -0600

    Commit to main

--- registry/bbt-website/www/home.php
+++ registry/bbt-website/www/home.php
@@ -237,7 +237,7 @@
         </a>
         </div>
         </div>
-        <div class="col-xl-4 col-sm-6 mb-4">
+        <div class="col-xl-3 col-sm-6 mb-3">
         <div class="card text-white bg-success o-hidden h-100">
         <div class="card-body">
         <div class="card-body-icon">
@@ -255,16 +255,31 @@
         </a>
         </div>
         </div>
-        <div class="col-xl-4 col-sm-6 mb-4">
+        <div class="col-xl-3 col-sm-6 mb-3">
         <div class="card text-white bg-danger o-hidden h-100">
         <div class="card-body">
         <div class="card-body-icon">
         <i class="fa fa-fw fa-support"></i>
         </div>
         
-        <!-- End of First Section Cards -->
-        
+        <div class="mr-5">13 New Tickets!</div>
+        </div>
+        <a class="card-footer text-white clearfix small z-1" href="#">
+        <span class="float-left">View Details</span>
+        <span class="float-right">
+        <i class="fa fa-angle-right"></i>
+        </span>
+        </a>
+        </div>
+        </div>
+        </div>
         <?php
+
+
+
+        // End of FIrst Section Cards
+        
+
         $servername = "10.200.1.91";
         $username = "admin";
         $password = "admin";
