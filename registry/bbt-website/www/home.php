eb2ce1c6eb6ebb6014e78e9cc25b1dfe0feb5923
smith.itpro@gmail.com <smith.itpro@gmail.com>
2024-04-14 08:27:35 -0600

    Commit to main

--- registry/bbt-website/www/home.php
+++ registry/bbt-website/www/home.php
@@ -167,155 +167,147 @@
 
 
         <div class="content-wrapper">
-        <div class="container-fluid">
-
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-        <a href="#">Dashboard</a>
-        </li>
-        <li class="breadcrumb-item active">My Dashboard</li>
-        </ol>
-
-        <!-- Icon Cards-->
-        <?php
-        $servername = "10.200.1.91";
-        $username = "admin";
-        $password = "admin";
-        $dbname = "bbt";
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        $sqll = "SELECT  * from sales_stats";
-        if (mysqli_query($conn, $sqll)) {
-            echo "";
-        } else {
-            echo "Error: " . $sqll . "<br>" . mysqli_error($conn);
-        }
-        $result = mysqli_query($conn, $sqll);
-        if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while ($row = mysqli_fetch_assoc($result)) { ?>
-
-
-
-
-        <!--  Total Jobs Completed -->
-        <div class="row">
-        <div class="col-xl-4 col-sm-6 mb-4">
-        <div class="card text-white bg-dark o-hidden h-100">
-        <div class="card-body">
-        <div class="card-body-icon">
-        <i class="fa fa-fw fa-comments"></i>
-        </div>
-        <div class="mr-5"><?php echo $row["Vistors"]; ?> Total Jobs Completed</div>
-        </div>
-        <a class="card-footer text-white clearfix small z-1" href="#">
-        <span class="float-left">View Details</span>
-        <span class="float-right">
-        <i class="fa fa-angle-right"></i>
-        </span>
-        </a>
-        </div>
-        </div>
-
-
-        <!-- Jobs in Queue -->
-        <div class="col-xl-4 col-sm-6 mb-4">
-        <div class="card text-white bg-dark o-hidden h-100">
-        <div class="card-body">
-        <div class="card-body-icon">
-        <i class="fa fa-fw fa-list"></i>
-        </div>
-        <div class="mr-5"><?php echo $row["revenue"]; ?>  Jobs in Queue</div>
-        </div>
-        <?php }
-        } else {
-            echo "0 results";
-        }
-        ?>
-        <a class="card-footer text-white clearfix small z-1" href="#">
-        <span class="float-left">View Details</span>
-        <span class="float-right">
-        <i class="fa fa-angle-right"></i>
-        </span>
-        </a>
-        </div>
-        </div>
-
-
-
-
-        <div class="content-wrapper">
-        <div class="container-fluid">
-
-        <!-- Active Jobs -->
-        <div class="col-xl-4 col-sm-6 mb-3">
-        <div class="card text-white bg-dark o-hidden h-100">
-        <div class="card-body">
-        <div class="card-body-icon">
-        <i class="fa fa-fw fa-shopping-cart"></i>
-        </div>
-        <div class="mr-5">4 Active Jobs</div>
-        </div>
-        <a class="card-footer text-white clearfix small z-1" href="#">
-        <span class="float-left">View Details</span>
-        <span class="float-right">
-        <i class="fa fa-angle-right"></i>
-        </span>
-        </a>
-        </div>
-        </div>
-
-        <!-- End of container-fluid-->
-        <!-- End of content-wrapper-->
-        </div>
-        </div>
-
-        <!-- Update Table Code -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script>
-            $(document).ready(function(){
-                function fetchJobActivity() {
-                    $.ajax({
-                        url: "fetch_job_activity.php",
-                        type: "GET",
-                        success: function(data){
-                            $("#dataTable tbody").html(data); // Replace the tbody contents
-                        }
-                    });
-                }
-
-                // Fetch job activity every 5 seconds
-                setInterval(fetchJobActivity, 5000); // Adjust time as needed
-
-                fetchJobActivity(); // Initial fetch
-            });
-        </script>
-            
-
-
-        <!-- Job Activity Card-->
-        <div class="card mb-3">
-            <div class="card-header text-white bg-dark">
-                <i class="fa fa-table"></i> Job Activity</div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                            <thead>
-                                <tr>
-                                    <th>JOB ID</th>
-                                    <th>Job Status</th>
-                                    <th>Last Update Date</th>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                </div>
-                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+            <div class="container-fluid">
+                <!-- Breadcrumbs-->
+                <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                <a href="#">Dashboard</a>
+                </li>
+                <li class="breadcrumb-item active">My Dashboard</li>
+                </ol>
             </div>
         </div>
 
+        <div class="content-wrapper">
+            <div class="container-fluid">
+                <!-- Icon Cards-->
+                <?php
+                $servername = "10.200.1.91";
+                $username = "admin";
+                $password = "admin";
+                $dbname = "bbt";
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                $sqll = "SELECT  * from sales_stats";
+                if (mysqli_query($conn, $sqll)) {
+                    echo "";
+                } else {
+                    echo "Error: " . $sqll . "<br>" . mysqli_error($conn);
+                }
+                $result = mysqli_query($conn, $sqll);
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while ($row = mysqli_fetch_assoc($result)) { ?>
+
+                <!--  Total Jobs Completed -->
+                <div class="row">
+                <div class="col-xl-4 col-sm-6 mb-4">
+                <div class="card text-white bg-dark o-hidden h-100">
+                <div class="card-body">
+                <div class="card-body-icon">
+                <i class="fa fa-fw fa-comments"></i>
+                </div>
+                <div class="mr-5"><?php echo $row["Vistors"]; ?> Total Jobs Completed</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+                </span>
+                </a>
+                </div>
+                </div>
+                
+                <!-- Jobs in Queue -->
+                <div class="col-xl-4 col-sm-6 mb-4">
+                <div class="card text-white bg-dark o-hidden h-100">
+                <div class="card-body">
+                <div class="card-body-icon">
+                <i class="fa fa-fw fa-list"></i>
+                </div>
+                <div class="mr-5"><?php echo $row["revenue"]; ?>  Jobs in Queue</div>
+                </div>
+                <?php }
+                } else {
+                    echo "0 results";
+                }
+                ?>
+                <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+                </span>
+                </a>
+                </div>
+                </div>
+
+                <!-- Active Jobs -->
+                <div class="col-xl-4 col-sm-6 mb-3">
+                <div class="card text-white bg-dark o-hidden h-100">
+                <div class="card-body">
+                <div class="card-body-icon">
+                <i class="fa fa-fw fa-shopping-cart"></i>
+                </div>
+                <div class="mr-5">4 Active Jobs</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1" href="#">
+                <span class="float-left">View Details</span>
+                <span class="float-right">
+                <i class="fa fa-angle-right"></i>
+                </span>
+                </a>
+                </div>
+                </div>
+            </div>
         </div>
+
+
+        
+        <div class="content-wrapper">
+            <div class="container-fluid">
+                <!-- Update Table Code -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+                <script>
+                    $(document).ready(function(){
+                        function fetchJobActivity() {
+                            $.ajax({
+                                url: "fetch_job_activity.php",
+                                type: "GET",
+                                success: function(data){
+                                    $("#dataTable tbody").html(data); // Replace the tbody contents
+                                }
+                            });
+                        }
+
+                        // Fetch job activity every 5 seconds
+                        setInterval(fetchJobActivity, 5000); // Adjust time as needed
+
+                        fetchJobActivity(); // Initial fetch
+                    });
+                </script>
+
+                <!-- Job Activity Card-->
+                <div class="card mb-3">
+                    <div class="card-header text-white bg-dark">
+                        <i class="fa fa-table"></i> Job Activity</div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>JOB ID</th>
+                                            <th>Job Status</th>
+                                            <th>Last Update Date</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                    </div>
+                </div>
+
+           </div>
         </div>
 
 
